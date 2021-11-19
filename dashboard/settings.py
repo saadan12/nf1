b055c038f7508e534fe30fc2fd50725f21fc2bbf
dani <saadan.waqar@triplek.tech>
2021-11-18 22:21:00 -0800

    heroku commit1

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -262,12 +262,12 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
