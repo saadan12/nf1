a7e8ca550ba4d2302c9f058377b057c7c8a444e6
saadan12 <93700894+saadan12@users.noreply.github.com>
2021-11-29 06:40:46 -0800

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, logout_view
+from .views import CreateCheckoutSessionView, UserRegisterView, logout_view
+from . import views
 
 urlpatterns = [
+    path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(),name="create-checkout-session"),
+    path('success/',views.confirm_payment, name="success"),
+    path('cancel/',views.cancel_payment, name="cancel"),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('logout/', logout_view, name='logout'),
 ]
