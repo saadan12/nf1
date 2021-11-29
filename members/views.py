3428819c6cc2e1df974328338333483390b4e663
saadan12 <93700894+saadan12@users.noreply.github.com>
2021-11-29 06:28:23 -0800

    Update views.py

--- members/views.py
+++ members/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
@@ -7,39 +7,43 @@ import stripe
 from django.conf import settings
 from django.http import JsonResponse
 from django.views import View
-
-
+from home.models import Post  
+ 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class CreateCheckoutSessionView(View):
+    
     def post(self, request, *args, **kwargs):
-        product_id = self.kwargs["pk"]
-        YOUR_DOMAIN = "http://localhost:8000"
+        post_id = self.kwargs["pk"]
+        post = Post.objects.get(id=post_id)
+        YOUR_DOMAIN = "http://nftion-test2.herokuapp.com"
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
                 {
                     'price_data': {
                         'currency': 'usd',
-                        'unit_amount': product.price,
+                        'unit_amount': post.asset_price ,
                         'product_data': {
-                            'name': product.name
+                            'name': "Test Payment"
                         },
                     },
                     'quantity': 1,
                 },
-            ],
-            metadata={
-                "product_id": product.id
-            },
+            ], 
             mode='payment',
-            success_url=YOUR_DOMAIN + '/success/',
-            cancel_url=YOUR_DOMAIN + '/cancel/',
+            success_url=YOUR_DOMAIN + '/members/success/',
+            cancel_url=YOUR_DOMAIN + '/members/cancel/',
         )
-        return JsonResponse({
-            'id': checkout_session.id
-        })
+         
+        return redirect(checkout_session.url, code=303)
+
+def confirm_payment(request):
+    return render(request, 'success.html')
+
+def cancel_payment(request):
+    return render(request, 'cancel.html')
 
 
 class UserRegisterView(generic.CreateView):
